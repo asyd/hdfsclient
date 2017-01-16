d29d8b2cfc69d693d6eda3389b74f9166cf9ed65
Bruno Bonfils <asyd@asyd.net>
2017-01-16 22:27:08 +0100

    First working version

---
+++ src/main/scala/io/kensu/Main.scala
@@ -0,0 +1,54 @@
+package io.kensu
+
+import com.beust.jcommander.{JCommander, Parameter, ParameterException, Parameters}
+
+/**
+  * Created by asyd on 09/01/17.
+  */
+
+object CommandLineInterface {
+  def main(args: Array[String]) {
+    //    var hdfsClient = new HDFSClient(args)
+    val parameters = new CommonParameters
+    // Don't ask me why, but new JCommander(parameters) doesn't work
+    val jCommander = new JCommander()
+    jCommander.addObject(parameters)
+
+    val commandLs:CommandLs = new CommandLs
+    jCommander.addCommand("ls", commandLs)
+
+    val commandFind:CommandFind = new CommandFind
+    jCommander.addCommand("find", commandFind)
+
+    try {
+      jCommander.parseWithoutValidation(args: _*)
+      val hdfsClient = new HDFSClient(parameters)
+      jCommander.getParsedCommand match {
+        case "ls" => hdfsClient.ls(commandLs.path)
+      }
+    } catch {
+      case _: ParameterException => jCommander.usage()
+    }
+  }
+}
+
+class CommonParameters {
+  @Parameter(names = Array("-v"), description = "Increase verbosity")
+  var verbose: Boolean = false
+
+  @Parameter(names = Array("--hdfsURL"), required = false)
+  var hdfsURL: String = null
+}
+
+@Parameters(commandDescription = "List directory content")
+class CommandLs {
+  @Parameter(names = Array("--path"), required = false)
+  var path: String = "/"
+}
+
+@Parameters(commandDescription = "Search for files in a directory hierarchy")
+class CommandFind {
+  @Parameter(required = false)
+  var path: String = "/"
+}
+
